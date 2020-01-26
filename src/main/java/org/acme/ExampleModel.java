950355796188bad0bc15d4bbc5b00442337a4fb0
dsg268 <ibrahim.abedalaziz@smartdubai.ae>
2020-01-26 08:48:38 +0400

    Fix messaging issue.

--- src/main/java/org/acme/ExampleModel.java
+++ src/main/java/org/acme/ExampleModel.java
@@ -1,24 +1,14 @@
 package org.acme;
 
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Emitter;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-@ApplicationScoped
 public class ExampleModel {
 
     public String msg;
-
-    @Inject 
-    @Channel("hello") 
-    public Emitter<String> ownerCreatedChannel;
 
     public ExampleModel() {
         super();
     }
 
     public void hello() { 
-        this.ownerCreatedChannel.send(this.msg);
+        //do your presistence logic here
     }
 }
--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,6 +1,7 @@
 package org.acme;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,15 +9,33 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.reactive.messaging.annotations.Channel;
+import io.smallrye.reactive.messaging.annotations.Emitter;
+
 @Path("/hello")
 @ApplicationScoped
 public class ExampleResource {
 
+	private final static Logger LOG = LoggerFactory.getLogger(ExampleResource.class);
+	
+	@Inject 
+    @Channel("hello") 
+    Emitter<String> ownerCreatedChannel;
+	
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response hello(ExampleModel example) {
-        example.hello();
+    	ownerCreatedChannel.send(example.msg);
         return Response.status(201).build();
+    }
+    
+    @Incoming("hello-incoming")
+    public void consumer(String msg) {
+    	LOG.info("Message {} has been consumed.", msg);
     }
 }
