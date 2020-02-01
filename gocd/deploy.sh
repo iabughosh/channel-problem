d841e1731cdad2bb538469f934f314c5be40b71a
dsg268 <ibrahim.abedalaziz@smartdubai.ae>
2020-02-02 00:21:37 +0400

    Cloud native CD.

--- gocd/deploy.sh
+++ gocd/deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker pull iabughosh/micro-channel
-docker run --rm -d -p 8080:8080 iabughosh/micro-channel
+docker run --rm -d -p 8080:8080 iabughosh/micro-channel:$GO_DEPENDENCY_LABEL_Build-docker-image
