624bd230e1132262dc09e10d58314564bdde5677
dsg268 <ibrahim.abedalaziz@smartdubai.ae>
2020-02-04 23:45:26 +0400

    try sudo

--- gocd/build-image.sh
+++ gocd/build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building & Pushing Docker Image."
 ./mvnw package
-docker build . -t iabughosh/micro-channel:$GO_PIPELINE_COUNTER
+sudo docker build . -t iabughosh/micro-channel:$GO_PIPELINE_COUNTER
